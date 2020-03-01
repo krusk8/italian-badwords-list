5c7bf6ee113088d19748a72168747be956aa6937
Mirko Tafuri <mirko.tafuri@conte.it>
2020-03-01 16:14:21 +0100

    added transform leet funciont, removing object, update array

--- test/exports.js
+++ test/exports.js
@@ -1,10 +1,6 @@
 var assert = require('better-assert');
 
 describe('filter', function(){
-  it('Should contain property object', function(){
-    var list = require('../lib/index');
-    assert(list.hasOwnProperty('object'));
-  });
 
   it('Should contain property array', function(){
     var list = require('../lib/index');
